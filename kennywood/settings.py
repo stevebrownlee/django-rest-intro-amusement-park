612ad1959acbdc894ebc7879e58c34feb74758fa
Steve Brownlee <chortlehoort@gmail.com>
2019-09-27 08:47:02 -0500

    Authenticate everything

--- kennywood/settings.py
+++ kennywood/settings.py
@@ -48,8 +48,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10
